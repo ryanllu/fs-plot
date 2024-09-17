630ca4cd3494c4128e061cc8a8a35038c3bf87ff
Cyiaostch <tejasatya1612@gmail.com>
2024-09-17 14:38:47 +0700

    README update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fsplot',
-    version='0.1',
+    version='0.2',
     description='`fsplot` is a Python library for lightweight visualization of financial market data using SVG.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
